6bf553311e78ad44f5bf0023d7e48adaf7bbcece
ALBERTO SALVATORE COLLETTO S270269 <s270269@studenti.polito.it>
2022-05-05 17:45:58 +0200

    v1 without control of columns

--- backend.js
+++ backend.js
@@ -2,6 +2,7 @@
 const fs = require('fs')
 
 
+
 try{
     if (process.argv.length != 5) throw "Parameters Missing"
     
@@ -10,10 +11,9 @@ try{
     const ntf = process.argv[4]
     //console.log(ntf.match(RegExp(/[a-zA-Z]+/)))
     if (!fs.existsSync(db_file) || !column.match(RegExp(/\b[0-9]+\b/)) || !ntf.match(RegExp(/\b[a-zA-Z]+\b/))) throw "Error Parameters"
-    let file = []
-    const f1 = fs.openSync(db_file, 0x111, 0x111)
-    
-    console.log(f1)    
+    const f1 = fs.readFileSync(db_file, {"encoding":"ascii"})
+    const f2 = f1.split("\r\n")
+    f2.forEach(x => {if (x.split(",")[column].match(ntf)) console.log(x)})
 }catch (e){
     console.error(e)
 }
